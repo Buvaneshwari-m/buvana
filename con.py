0162989b45734a8e8718a501d8eb25fb8d86e118
Buvaneshwari-m <52272256+Buvaneshwari-m@users.noreply.github.com>
2019-07-01 11:05:56 +0530

    Create con.py

---
+++ con.py
@@ -0,0 +1,6 @@
+
+ch=input("Enter the number")
+if(ch=='a' or ch=='i'or ch=='e'or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I'or ch=='O' or ch=='U'):
+	print("Vowel")
+else:
+	print("Consonant")
